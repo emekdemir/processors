b44f05e2c6be7d85be8a5b8525648237d0e20784
Dane Bell <dane@email.arizona.edu>
2015-03-27 14:44:15 -0700

    allowing withNER=false

--- src/main/scala/edu/arizona/sista/odin/impl/Dependencies.scala
+++ src/main/scala/edu/arizona/sista/odin/impl/Dependencies.scala
@@ -16,8 +16,9 @@ trait Dependencies {
   def outgoingEdges(sent: Int, doc: Document): Array[Array[(Int, String)]] =
     dependencies(sent, doc).outgoingEdges
 
-  def incoming(tok: Int, sent: Int, doc: Document): Seq[String] =
+  def incoming(tok: Int, sent: Int, doc: Document): Seq[String] = {
     incomingEdges(sent, doc).lift(tok).getOrElse(Array.empty).map(_._2)
+  }
 
   def outgoing(tok: Int, sent: Int, doc: Document): Seq[String] =
     outgoingEdges(sent, doc).lift(tok).getOrElse(Array.empty).map(_._2)
